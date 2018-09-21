ef9ca45cace928bd0566609e7ee924e615969f4b
undersourcecode <undersourcecode@hotmail.co.jp>
2018-09-21 10:34:51 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# go-learning-firestore-samples
+
+## About This
+
+Goolge Firestoreの学習用サンプルソース。
+
+### datamodel
+
+https://firebase.google.com/docs/firestore/data-model?authuser=0
+「サブコレクション」の登録をGolangで再現したもの。
---
+++ datamodel/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+type Room struct {
+	Name string `firestore:"name,omitempty"`
+}
+
+type Message struct {
+	From string `firestore:"from,omitempty"`
+	Msg  string `firestore:"msg,omitempty"`
+}
+
+func main() {
+	fmt.Println("----- main start -----")
+
+	jsonPath := os.Getenv("FIREBASE_JSON_PATH")
+
+	opt := option.WithCredentialsFile(jsonPath)
+	ctx := context.Background()
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	err = create(ctx, client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = reference(ctx, client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("----- main end -----")
+}
+
+func reference(ctx context.Context, client *firestore.Client) error {
+	messageRef := client.Collection("rooms").Doc("roomA").
+		Collection("messages").Doc("message1")
+
+	snapshot, err := messageRef.Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(snapshot.Data())
+	return nil
+}
+
+func create(ctx context.Context, client *firestore.Client) error {
+	roomA := Room{
+		Name: "my chat room",
+	}
+	message1 := Message{
+		From: "alex",
+		Msg:  "Hello World!",
+	}
+	_, err := client.Collection("rooms").Doc("roomA").Set(ctx, roomA)
+	if err != nil {
+		return err
+	}
+	_, err = client.Collection("rooms").Doc("roomA").Collection("messages").Doc("message1").Set(ctx, message1)
+	if err != nil {
+		return err
+	}
+
+	message2 := Message{
+		From: "tom",
+		Msg:  "Hello Tom!",
+	}
+	_, err = client.Collection("rooms").Doc("roomA").Collection("messages").Doc("message2").Set(ctx, message2)
+	if err != nil {
+		return err
+	}
+
+	roomB := Room{
+		Name: "beta room",
+	}
+	message3 := Message{
+		From: "ken",
+		Msg:  "Hello Ken!",
+	}
+	_, err = client.Collection("rooms").Doc("roomB").Set(ctx, roomB)
+	if err != nil {
+		return err
+	}
+	_, err = client.Collection("rooms").Doc("roomB").Collection("messages").Doc("message3").Set(ctx, message3)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
